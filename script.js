2414b76716641bb000a211cf64f72c02da3d3d1b
ClaseMINTIC2024 <ing_nidiag@hotmail.com>
2024-11-18 16:36:18 -0500

    FINAL

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.getElementById('nav-toggle').addEventListener('click', function(){
-    document.querySelector('nav').classList.toggle('nav-open');
+document.addEventListener("DOMContentLoaded", () => {
+    const navToggle = document.getElementById("nav-toggle");
+    const nav = document.querySelector("nav");
+
+    navToggle.addEventListener("change", () => {
+        nav.classList.toggle("nav-open");
+    });
 });
