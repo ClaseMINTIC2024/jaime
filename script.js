62b89c51bbaeeab82183a601355c7154b630fccb
ClaseMINTIC2024 <ing_nidiag@hotmail.com>
2024-11-19 09:16:37 -0500

    prueba

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const navToggle = document.getElementById("nav-toggle");
-    const nav = document.querySelector("nav");
-
-    navToggle.addEventListener("change", () => {
-        nav.classList.toggle("nav-open");
-    });
-});
+function mostrarMensaje(sesion){
+    alert('Este es un mensaje desde la '+sesion);
+}
