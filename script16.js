2a22b7c27e61e4e6222d17f1b9a5ca440e3411ea
ClaseMINTIC2024 <ing_nidiag@hotmail.com>
2024-11-19 15:21:09 -0500

    scr

---
+++ script16.js
@@ -0,0 +1,14 @@
+function validarFormulario(){
+    let nombre=document.getElementById("nombre").Value;
+    let email=document.getElementById("email").Value;
+    let iamgen=document.getElementById("imagen").Value;
+    if(nombre=="" || email=="" || imagen=="" ){
+        alert("todos los campos son obligatorios");
+        return false;
+    }
+    return true;
+}
+function mostrarImagen(event){
+    let imagen=document.getElementById('ver-imagen');
+    imagen.src=URL.createObjectURL(event.target.files[0]);
+}
